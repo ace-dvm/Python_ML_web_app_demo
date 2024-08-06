180222194ed81362e3208021a251c146768125a3
ace-dvm <ace.medlock@gmail.com>
2024-08-06 11:16:42 +0200

    use correct version of database file

--- database.sql
+++ database.sql
@@ -9,16 +9,15 @@ CREATE TABLE `patient` (
   `BMI` float(3),
   `diab_pedigree` float(4),
   `age` int unsigned,
-  `prediction` float(4),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 insert into patient values
-(null, "Susan Foreman",6,148,72,35,0,33.6,0.627,50,null),
-(null, "Sarah Jane Smith",2,106,56,27,165,29.0,0.426,22,null),
-(null, "Tegan Jovanka",2,174,88,37,120,44.5,0.646,24,1,null),
-(null, "Amy Pond",4,95,60,32,0,35.4,0.284,28,null),
-(null, "Yasmin Khan",0,126,86,27,120,27.4,0.515,21,null)
+(null, "Susan Foreman",6,148,72,35,0,33.6,0.627,50),
+(null, "Sarah Jane Smith",2,106,56,27,165,29.0,0.426,22),
+(null, "Tegan Jovanka",2,174,88,37,120,44.5,0.646,24,1),
+(null, "Amy Pond",4,95,60,32,0,35.4,0.284,28),
+(null, "Yasmin Khan",0,126,86,27,120,27.4,0.515,21)
 ;
 
 
